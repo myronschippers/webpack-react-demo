ed03a0f9c08ee598025f4036dc094c85786f6cbc
Myron <dubtwee@gmail.com>
2019-05-04 12:25:36 -0500

    adding working development structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('appJump'));
